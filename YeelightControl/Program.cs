3073656085fd4e23ab9a9b1f0933444ba04d6b48
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2023-06-13 06:43:53 +0800

    Change to auto detect.

--- YeelightControl/Program.cs
+++ YeelightControl/Program.cs
@@ -4,31 +4,30 @@ using System.Threading.Tasks;
 using System.Linq;
 using YeelightAPI;
 using YeelightAPI.Models.ColorFlow;
-using System.Diagnostics;
 
 namespace YeelightControl {
 	class Program {
-		static List<string> LoadBulbs() {
-			var bulbs = new List<string>();
-			try {
-				bulbs = JsonOperation.LoadData();
-			} catch (Exception ex) {
-				Output.Error(ex.Message);
-			}
-			return bulbs;
-		}
-
-		static List<Device> GetDevicesList() {
-			var bulbs = LoadBulbs();
-			return bulbs.Select(bulb => new Device(bulb)).ToList();
-		}
-
 		static void StartFlow(List<Device> devices, ColorFlow flow) {
 			devices.ForEach(async device => await device.StopColorFlow());
 			devices.ForEach(async device => await device.StartColorFlow(flow));
 		}
 
+		static void StartFlow(DeviceGroup devices, ColorFlow flow) {
+			devices.StopColorFlow();
+			devices.StartColorFlow(flow);
+		}
+
 		static void RGBFlow(List<Device> devices, int brightness) {
+			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
+				new ColorFlowRGBExpression(255, 0, 0, brightness, 2000),
+				new ColorFlowRGBExpression(0, 255, 0, brightness, 2000),
+				new ColorFlowRGBExpression(0, 0, 255, brightness, 2000),
+			};
+
+			StartFlow(devices, flow);
+		}
+
+		static void RGBFlow(DeviceGroup devices, int brightness) {
 			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
 				new ColorFlowRGBExpression(255, 0, 0, brightness, 2000),
 				new ColorFlowRGBExpression(0, 255, 0, brightness, 2000),
@@ -46,6 +45,14 @@ namespace YeelightControl {
 			StartFlow(devices, flow);
 		}
 
+		static void ChangeTempFlow(DeviceGroup devices, int temperature, int brightness) {
+			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
+				new ColorFlowTemperatureExpression(temperature, brightness, 1000)
+			};
+
+			StartFlow(devices, flow);
+		}
+
 		static void ResetFlow(List<Device> devices, int brightness) {
 			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
 				new ColorFlowTemperatureExpression(4500, brightness, 1000)
@@ -54,10 +61,19 @@ namespace YeelightControl {
 			StartFlow(devices, flow);
 		}
 
-		static void Run(List<string> args) {
+		static void ResetFlow(DeviceGroup devices, int brightness) {
+			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
+				new ColorFlowTemperatureExpression(4500, brightness, 1000)
+			};
+
+			StartFlow(devices, flow);
+		}
+
+		static async Task Run(List<string> args) {
 			if (!ArgsValidation.Check(args)) return;
 
-			var deviceList = GetDevicesList();
+			var deviceList = (await DeviceLocator.DiscoverAsync()).ToList();
+			// var devices = new DeviceGroup(await DeviceLocator.DiscoverAsync());
 
 			if (deviceList.Count == 0) {
 				Output.NoBulb();
@@ -66,18 +82,24 @@ namespace YeelightControl {
 
 			try {
 				deviceList.ForEach(async device => await device.Connect());
+				// await devices.Connect();
+
 				switch (args[0]) {
 					case (Commands.On):
-						deviceList.ForEach(async device => await device.TurnOn());
+						deviceList.ForEach(async device => await device.TurnOn(1000));
+						// await devices.TurnOn(1000);
 						break;
 					case (Commands.Off):
-						deviceList.ForEach(async device => await device.TurnOff());
+						deviceList.ForEach(async device => await device.TurnOff(1000));
+						// await devices.TurnOff(1000);
 						break;
 					case (Commands.Set):
 						deviceList.ForEach(async device => await device.SetBrightness(Convert.ToInt32(args[1]), 1000));
+						// await devices.SetBrightness(Convert.ToInt32(args[1]), 1000);
 						break;
 					case (Commands.Toggle):
 						deviceList.ForEach(async device => await device.Toggle());
+						// await devices.Toggle();
 						break;
 					case (Commands.RGB):
 						RGBFlow(deviceList, Convert.ToInt32(args[1]));
@@ -91,13 +113,14 @@ namespace YeelightControl {
 				}
 
 				deviceList.ForEach(device => device.Disconnect());
+				// devices.Disconnect();
 			} catch (Exception ex) {
 				Output.Error(ex.Message);
 			}
 		}
 
-		static void Main(string[] args) {
-			Run(args.Select(x => x.ToLower()).ToList());
+		static async Task Main(string[] args) {
+			await Run(args.Select(x => x.ToLower()).ToList());
 		}
 	}
 }
