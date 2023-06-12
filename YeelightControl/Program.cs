986aa606347d9ce32d94f7c3ea5a5527316ea151
azhuge233 <17763056+azhuge233@users.noreply.github.com>
2023-06-13 05:22:33 +0800

    Update fixes nothing. Change DeviceGroup to List<Device> make the program from "not working at all" to "needs luck to work".

--- YeelightControl/Program.cs
+++ YeelightControl/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using System.Linq;
 using YeelightAPI;
 using YeelightAPI.Models.ColorFlow;
+using System.Diagnostics;
 
 namespace YeelightControl {
 	class Program {
@@ -17,95 +18,86 @@ namespace YeelightControl {
 			return bulbs;
 		}
 
-		static DeviceGroup GetDevices() {
+		static List<Device> GetDevicesList() {
 			var bulbs = LoadBulbs();
-			var devicesGroup = new DeviceGroup();
-			foreach (var bulb in bulbs) {
-				var device = new Device(bulb);
-				devicesGroup.Add(device);
-			}
-			return devicesGroup;
+			return bulbs.Select(bulb => new Device(bulb)).ToList();
 		}
 
-		static async Task RGBFlow(DeviceGroup devices, int brightness) {
+		static void StartFlow(List<Device> devices, ColorFlow flow) {
+			devices.ForEach(async device => await device.StopColorFlow());
+			devices.ForEach(async device => await device.StartColorFlow(flow));
+		}
+
+		static void RGBFlow(List<Device> devices, int brightness) {
 			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
 				new ColorFlowRGBExpression(255, 0, 0, brightness, 2000),
 				new ColorFlowRGBExpression(0, 255, 0, brightness, 2000),
 				new ColorFlowRGBExpression(0, 0, 255, brightness, 2000),
 			};
 
-			await devices.StopColorFlow();
-			await devices.StartColorFlow(flow);
+			StartFlow(devices, flow);
 		}
 
-		static async Task ChangeTempFlow(DeviceGroup devices, int temperature, int brightness) {
+		static void ChangeTempFlow(List<Device> devices, int temperature, int brightness) {
 			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
 				new ColorFlowTemperatureExpression(temperature, brightness, 1000)
 			};
 
-			await devices.StopColorFlow();
-			await devices.StartColorFlow(flow);
+			StartFlow(devices, flow);
 		}
 
-		static async Task ResetFlow(DeviceGroup devices, int brightness) {
+		static void ResetFlow(List<Device> devices, int brightness) {
 			ColorFlow flow = new(0, ColorFlowEndAction.Keep) {
 				new ColorFlowTemperatureExpression(4500, brightness, 1000)
 			};
 
-			await devices.StopColorFlow();
-			await devices.StartColorFlow(flow);
+			StartFlow(devices, flow);
 		}
 
-		static async Task Run(List<string> args) {
+		static void Run(List<string> args) {
 			if (!ArgsValidation.Check(args)) return;
 
-			var devices = GetDevices();
+			var deviceList = GetDevicesList();
 
-			if (devices.Count == 0) {
+			if (deviceList.Count == 0) {
 				Output.NoBulb();
 				return;
 			}
 
-			// two try-catch in case of one or more bulb failures
 			try {
-				await devices.Connect();
-			} catch (Exception ex) {
-				Output.Error(ex.Message);
-			}
-
-			try {
+				deviceList.ForEach(async device => await device.Connect());
 				switch (args[0]) {
 					case (Commands.On):
-						await devices.TurnOn(1000);
+						deviceList.ForEach(async device => await device.TurnOn());
 						break;
 					case (Commands.Off):
-						await devices.TurnOff(1000);
+						deviceList.ForEach(async device => await device.TurnOff());
 						break;
 					case (Commands.Set):
-						await devices.SetBrightness(Convert.ToInt32(args[1]), 1000);
+						deviceList.ForEach(async device => await device.SetBrightness(Convert.ToInt32(args[1]), 1000));
 						break;
 					case (Commands.Toggle):
-						await devices.Toggle();
+						deviceList.ForEach(async device => await device.Toggle());
 						break;
 					case (Commands.RGB):
-						await RGBFlow(devices, Convert.ToInt32(args[1]));
+						RGBFlow(deviceList, Convert.ToInt32(args[1]));
 						break;
 					case (Commands.Temperature):
-						await ChangeTempFlow(devices, Convert.ToInt32(args[1]), Convert.ToInt32(args[2]));
+						ChangeTempFlow(deviceList, Convert.ToInt32(args[1]), Convert.ToInt32(args[2]));
 						break;
 					case (Commands.Reset):
-						await ResetFlow(devices, Convert.ToInt32(args[1]));
+						ResetFlow(deviceList, Convert.ToInt32(args[1]));
 						break;
 				}
+
+				deviceList.ForEach(device => device.Disconnect());
 			} catch (Exception ex) {
 				Output.Error(ex.Message);
-			} finally {
-				devices.Disconnect();
 			}
 		}
 
-		static async Task Main(string[] args) {
-			await Run(args.Select(x => x.ToLower()).ToList());
+		static void Main(string[] args) {
+			Run(args.Select(x => x.ToLower()).ToList());
 		}
 	}
 }
